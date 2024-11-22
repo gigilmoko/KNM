cea8ebb17bfb19197d36733b0c2517cfa03dadcd
gigilmoko <gregorijiselle2008@gmail.com>
2024-11-23 00:55:06 +0800

    Refactor JWT token utility: remove unnecessary logging and response properties for cleaner token handling

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -1,21 +1,15 @@
-const sendToken = (user, statusCode, res) => {
-    console.log(user);
+const token = user.getJwtToken();
+const options = {
+    expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+}
+res.status(statusCode).cookie('token', token, options).json({
+    success: true,
+    token,
+    user
+})
 
-    const token = user.getJwtToken();
-    const options = {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true,
-    };
-
-    res.status(statusCode).cookie('token', token, options).json({
-        success: true,
-        token,
-        user,
-        url: "/",
-        methods: ['GET', 'POST', 'PUT', 'OPTIONS', 'DELETE'],
-    });
-};
 
 module.exports = sendToken;
