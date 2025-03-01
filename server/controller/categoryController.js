7f0fde8a7fcaa537fb8fb437eedab50d17436c99
miryan1ez <veeveepeexd@gmail.com>
2025-03-01 22:49:54 +0800

    package json

--- server/controller/categoryController.js
+++ server/controller/categoryController.js
@@ -1,5 +1,5 @@
 const Category = require('../models/category')
-
+const Product = require('../models/product')
 exports.getCategory = async (req, res, next) => {
 	try {
 		const categories = await Category.find();
@@ -92,6 +92,34 @@ exports.deleteCategory = async (req, res, next) => {
 		message: 'Category deleted'
 	})
 };
+
+// exports.getCategoryWithProducts = async (req, res, next) => {
+//     try {
+//         const { categoryId } = req.params;
+
+//         // Find category
+//         const category = await Category.findById(categoryId);
+//         if (!category) {
+//             return res.status(404).json({
+//                 success: false,
+//                 message: 'Category not found'
+//             });
+//         }
+
+//         // Find products in this category
+//         const products = await Product.find({ category: categoryId });
+
+//         res.status(200).json({
+//             success: true,
+//             category,
+//             products,
+//             count: products.length
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// };
+
 
 // exports.getAdminCategories = async (req, res, next) => {
 // 	const categories = await Category.find();
--- server/routes/category.js
+++ server/routes/category.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { isAuthenticatedUser, authorizeRoles, isAdmin } = require('../middlewares/auth');
 
-const { getCategory, newCategory, getSingleCategory, deleteCategory, updateCategory } = require('../controller/categoryController');
+const { getCategory, newCategory, getSingleCategory, deleteCategory, updateCategory, getCategoryWithProducts} = require('../controller/categoryController');
 
 router.get('/category/all', getCategory);
 router.post('/category/new', isAuthenticatedUser, isAdmin, newCategory);
 router.get('/category/:id', getSingleCategory);
+// router.get('/category/:categoryId/products', getCategoryWithProducts);
 router.delete('/category/delete/:id', isAuthenticatedUser, isAdmin, deleteCategory);
 router.put('/category/update/:id', isAuthenticatedUser, isAdmin, updateCategory);
 
